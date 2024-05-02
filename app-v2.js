b13a48b6c766e49839bf933f22373ecba8b5b763
Opinaka <52041843+opinaka@users.noreply.github.com>
2024-05-02 11:47:19 +0200

    Update app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -53,48 +53,6 @@ app.get('/webhooks', (req, res) => {
   res.json(receivedMessages);
 });
 
-// Configuration de Swagger
-const options = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Webhook API endpoint',
-      version: '1.0.0',
-      description: 'L\'API Webhook Endpoint permet à votre application de recevoir des notifications en temps réel sur des événements spécifiques, ce qui peut être utile pour déclencher des actions ou des mises à jour en fonction de ces événements.',
-    },
-    servers: [
-      {
-        url: 'https://cours-ws-app-ex06.onrender.com',
-      },
-    ],
-  },
-  apis: ['server2.js'],
-};
-
-const specs = swaggerJsdoc(options);
-
-// Personnalisation du rendu de Swagger UI
-const swaggerOptions = {
-  customCss: `
-    .swagger-ui .topbar {
-      background-image: url('https://raw.githubusercontent.com/opinaka/cours-ws-app-ex06/main/ynov.png');
-      background-repeat: no-repeat;
-      background-size: contain;
-      background-position: center;
-      height: 50px; /* Ajustez la hauteur en fonction de votre logo */
-    }
-
-    .swagger-ui .topbar .link {
-      display: none; /* Masquer le logo Swagger par défaut */
-    }
-  `,
-  customSiteTitle: "Opinaka",
-  customfavIcon: "https://raw.githubusercontent.com/opinaka/cours-ws-app-ex06/main/ynov.png"
-};
-
-
-app.use('/', swaggerUi.serve, swaggerUi.setup(specs, swaggerOptions));
-
 // Démarrer le serveur sur le port de votre choix
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
